d024197f4ccc8acaa18c0b31680d5f2a15f91ad9
kaushikkrp <38283597+kaushikkrp@users.noreply.github.com>
2018-04-12 15:17:23 +0530

    Update Apptest.java

--- src/test/java/com/qaagility/controller/Apptest.java
+++ src/test/java/com/qaagility/controller/Apptest.java
@@ -1,1 +1,44 @@
+package com.qaagility.controller;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import com.qaagility.controller.Calculator;
+ 
+
+public class AppTest {
+
+Calculator myCalc = new Calculator();
+
+      public void testSlow() {
+        System.out.println("slow");
+      }
+
+      public void testSlower() {
+        System.out.println("slower");
+      }
+
+      public void testFast() {
+        System.out.println("fast-updated");
+      }
+	
+	@Test	
+      public void testCalc() {
+        assertEquals("Result", 9, myCalc.add(3,6));
+      }
+
+    }
+
+    © 2018 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    API
+    Training
+    Shop
+    Blog
+    About
 
